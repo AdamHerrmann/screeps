7e3739b21589760dee8c2cd9fde4a8f38230f582
Adam Herrmann <Adam.Herrmann@gmail.com>
2016-07-16 15:16:15 -0500

    fix(spawn): Remove harvester spawning code

--- main.js
+++ main.js
@@ -7,11 +7,6 @@ const roles = {
 module.exports.loop = function () {
   cleanupMemory();
 
-  const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === roles.harvester);
-  if (harvesters.length < 2) {
-    Game.spawns.home.createCreep([WORK,CARRY,MOVE], undefined, {role: roles.harvester})
-  }
-
   for(const name in Game.creeps) {
     const creep = Game.creeps[name];
 
