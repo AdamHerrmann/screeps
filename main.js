020c2c05c26a9223936037bb516ab61810b5fe29
Adam Herrmann <Adam.Herrmann@gmail.com>
2016-07-16 14:55:05 -0500

    fix(all): Try again

--- main.js
+++ main.js
@@ -1,33 +1,26 @@
 'use strict';
 
-const roleHarvester = require('role.harvester');
+const roles = {
+  harvester: require('role.harvester'),
+};
 
 module.exports.loop = function () {
+  cleanupMemory();
 
-    // Always place this memory cleaning code at the very top of your main loop!
+  const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === roles.harvester);
+  if (harvesters.length < 2) {
+    Game.spawns.home.createCreep([WORK,CARRY,MOVE], undefined, {role: role.harvester})
+  }
 
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
-        }
-    }
+  for(const creep of Game.creeps) {
+    creep.memory.role(creep);
+  }
+}
 
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
-
-    if(harvesters.length < 2) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-        console.log('Spawning new harvester: ' + newName);
-    }
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-    }
+function cleanupMemory () {
+  _.chain(Memory.creeps)
+    .reject((creep) => Game.creeps[creep.name])
+    .each((creep) => delete Memory.creeps[creep.name])
+    .value()
+  ;
 }
