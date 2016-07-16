6eeb5451749864e2d59b35344740076cd4f0a855
Adam Herrmann <Adam.Herrmann@gmail.com>
2016-07-16 14:35:36 -0500

    feat(harvester): Add basic harvester

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-'use strict'
+'use strict';
 
-module.exports.loop = loop;
+const roleHarvester = require('roles/harvester');
 
-function loop() {
+module.exports.loop = function () {
 
+    // Always place this memory cleaning code at the very top of your main loop!
 
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if(harvesters.length < 2) {
+        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+    }
 }
